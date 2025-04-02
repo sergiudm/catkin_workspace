fa11a75a7f93460075dd9b82816fdb31b0e241db
qijunhan <sergiudm@outlook.com>
2025-04-02 17:56:35 +0800

    update unit

--- src/fwd_kinematics/src/transform.cpp
+++ src/fwd_kinematics/src/transform.cpp
@@ -1,6 +1,5 @@
 #include <Eigen/Core>
 #include <Eigen/Dense>
-#include <Eigen/src/Core/Matrix.h>
 #include <cmath>
 #include <iostream>
 #include <vector>
@@ -21,7 +20,7 @@ Eigen::Matrix4d transform(Eigen::Vector4d DH_table) {
   T(1, 0) = sin(theta) * cos(alpha);
   T(1, 1) = cos(theta) * cos(alpha);
   T(1, 2) = -sin(alpha);
-  T(1, 3) = -1 * sin(alpha) * d;
+  T(1, 3) = -sin(alpha) * d;
 
   T(2, 0) = sin(theta) * sin(alpha);
   T(2, 1) = cos(theta) * sin(alpha);
@@ -37,15 +36,15 @@ Eigen::Matrix4d transform(Eigen::Vector4d DH_table) {
 }
 
 Eigen::Matrix4d T_base2tool(Eigen::VectorXd thetas) {
-  Eigen::Matrix4d T = Eigen::Matrix4d::Identity();
-  Eigen::Vector4d l1 = {M_PI / 2.0, 0, 128.3 + 115.0, thetas[0]};
-  Eigen::Vector4d l2 = {M_PI, 280, 30, thetas[1] + M_PI / 2.0};
-  Eigen::Vector4d l3 = {M_PI / 2.0, 0, 20, thetas[2] + M_PI / 2.0};
-  Eigen::Vector4d l4 = {M_PI / 2.0, 0, 140.0 + 105.0, thetas[3] + M_PI / 2.0};
-  Eigen::Vector4d l5 = {M_PI / 2.0, 0, 28.5 + 28.5, thetas[4] + M_PI};
-  Eigen::Vector4d l6 = {0, 0, 105.0 + 130.0, thetas[5] + M_PI / 2.0};
-
+  Eigen::Vector4d l1 = {M_PI / 2.0, 0, 0.1283 + 0.115, thetas[0]};
+  Eigen::Vector4d l2 = {M_PI, 0.280, 0.030, thetas[1] + M_PI / 2.0};
+  Eigen::Vector4d l3 = {M_PI / 2.0, 0, 0.020, thetas[2] + M_PI / 2.0};
+  Eigen::Vector4d l4 = {M_PI / 2.0, 0, 0.140 + 0.105, thetas[3] + M_PI / 2.0};
+  Eigen::Vector4d l5 = {M_PI / 2.0, 0, 0.0285 + 0.0285, thetas[4] + M_PI};
+  Eigen::Vector4d l6 = {0, 0, 0.105 + 0.130, thetas[5] + M_PI / 2.0};
   std::vector<Eigen::Vector4d> DH_tables = {l1, l2, l3, l4, l5, l6};
+
+  Eigen::Matrix4d T = Eigen::Matrix4d::Identity();
   for (int i = 0; i < DH_tables.size(); i++) {
     T = T * transform(DH_tables[i]);
   }
@@ -66,7 +65,15 @@ Eigen::VectorXd transformation(Eigen::VectorXd thetas) {
   result(4) =
       atan2(-T(2, 0), sqrt(T(2, 1) * T(2, 1) + T(2, 2) * T(2, 2))); // pitch
   result(5) = atan2(T(2, 1), T(2, 2));                              // yaw
-  // Convert radians to degrees
+
+  for (int i = 0; i < 3; ++i) {
+    result[i] *= 1000;
+  }
+
+  for (int i = 3; i < 6; ++i) {
+    result[i] *= 180 / M_PI;
+  }
+
   return result;
 }
 
@@ -78,10 +85,19 @@ int main() {
   Eigen::VectorXd test3(6);
   test3 << 357, 21, 150, 272, 320, 273;
 
-  auto T1 = transformation(test1);
-  auto T2 = transformation(test2);
-  auto T3 = transformation(test3);
-  std::cout << "T1: " << T1.transpose() << std::endl;
-  std::cout << "T2: " << T2.transpose() << std::endl;
-  std::cout << "T3: " << T3.transpose() << std::endl;
+  auto t1 = T_base2tool(test1);
+  auto t2 = T_base2tool(test2);
+  auto t3 = T_base2tool(test3);
+
+  auto p1 = transformation(test1);
+  auto p2 = transformation(test2);
+  auto p3 = transformation(test3);
+
+  std::cout << "t1: " << t1 << std::endl;
+  std::cout << "t2: " << t2 << std::endl;
+  std::cout << "t3: " << t3 << std::endl;
+
+  std::cout << "p1: " << p1.transpose() << std::endl;
+  std::cout << "p2: " << p2.transpose() << std::endl;
+  std::cout << "p3: " << p3.transpose() << std::endl;
 }
